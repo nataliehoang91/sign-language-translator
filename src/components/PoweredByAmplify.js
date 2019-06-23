16fca8bc0a54c2e779454a61464beaf52b158a23
khoi <khoiracle@gmail.com>
2019-06-23 10:51:50 +0700

    Powered by Amplify

--- src/components/PoweredByAmplify.js
+++ src/components/PoweredByAmplify.js
@@ -2,10 +2,8 @@ import React, { Component } from "react";
 
 function Amplify(props) {
   return (
-    <nav class="navbar fixed-bottom navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        <img src="https://camo.githubusercontent.com/8ec35c70c818d5c0031e347e63ec857f6d57f932/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6177732d6d6f62696c652d6875622d696d616765732f6177732d616d706c6966792d6c6f676f2e706e67" />
-      </a>
+    <nav class="fixed-bottom  footer-bg text-center">
+      <img className="awslogo" src="img/aws.png" />{" "}
     </nav>
   );
 }
