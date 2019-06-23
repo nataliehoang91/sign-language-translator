8bad549ca65d28c4f29596fe14681ac9efbfac17
khoi <khoiracle@gmail.com>
2019-06-23 08:55:05 +0700

    Add Amplify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import './App.css';
-import Navigation from './components/Navigation'
-import TextDisplay from './components/TextDisplay'
-import WebcamDisplay from './components/WebcamArea'
-import Title from './components/Description'
+import React from "react";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import TextDisplay from "./components/TextDisplay";
+import WebcamDisplay from "./components/WebcamArea";
+import Title from "./components/Description";
+import Auth from "@aws-amplify/auth";
+import Analytics from "@aws-amplify/analytics";
+import awsconfig from "./aws-exports";
 
-function App() {
-  return (
-    <div className="container">
-      <Navigation />
-      <Title/>
-      <div>
-        <TextDisplay />
+Auth.configure(awsconfig);
+Analytics.configure(awsconfig);
 
+class App extends React.Component {
+  componentDidMount() {
+    Analytics.record("app_start").then(evt => {
+      console.log(evt);
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Navigation />
+        <Title />
+        <div>
+          <TextDisplay />
+        </div>
+        <div>
+          <WebcamDisplay />
+        </div>
       </div>
-      <div>
-      <WebcamDisplay/>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
